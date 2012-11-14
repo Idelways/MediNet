cf48f3203951c55963176939c3989e2d30502851
Rayid Tayeb-Rahmani <idelways@gmail.com>
2012-11-14 22:00:14 +0100

    App generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MediNet::Application.config.secret_token = '41f31930bb6936d7850a32d5b35b716cf8672885ab5ea151d89c7c53db1dd4ab75a8fa7dd6b42db25a847f0f1d13bef045f81545cc3e796214c66ed1b527ad5f'
